c735f5737bf39ac6c44537af712a9a16bf648262
Aditya Gangil <111217346+adityagangil@users.noreply.github.com>
2023-08-21 19:01:23 +0530

    Add files via upload

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,12 +1,19 @@
-function ExpenseItem() {
+import React from 'react';
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
     return (
-        <div>
-            <div><h1>Expense Items</h1></div>
-            <h3>Food Rs 10</h3>
-            <h3>Petrol Rs 100</h3>
-            <h3>Movies Rs 200</h3>
+        <div className="expense-item">
+            <div>{props.date.toDateString()}</div>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+            <div className="expense-item__location">
+                Location: {props.location}
+            </div>
         </div>
     );
 }
 
-export default ExpenseItem 
+export default ExpenseItem;
