948bd91e4b82b551c32489318631883eec9ebd85
Aditya Gangil <111217346+adityagangil@users.noreply.github.com>
2023-08-29 10:02:41 +0530

    Add files via upload

--- ExpenseForm.js
+++ ExpenseForm.js
@@ -44,22 +44,25 @@ const ExpenseForm = (props) => {
         const expenseData = {
             title: enteredTitle,
             amount: enteredAmount,
-            date: new Date(enteredDate)
+            date: new Date(enteredDate),
         };
+
         props.onSaveExpenseData(expenseData);
         setEnteredTitle('');
         setEnteredAmount('');
         setEnteredDate('');
-    }
+    };
 
     return (
         <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text'
+                    <input
+                        type='text'
                         value={enteredTitle}
-                        onChange={titleChangeHandler} />
+                        onChange={titleChangeHandler}
+                    />
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
@@ -83,6 +86,7 @@ const ExpenseForm = (props) => {
                 </div>
             </div>
             <div className='new-expense__actions'>
+                <button type="button" onClick={props.onCancel}>Cancel</button>
                 <button type='submit'>Add Expense</button>
             </div>
         </form>
